4dcdc7ad7e7d6b4b02e9c8ce1cad77af43eed246
Bill Britt <bill@Bills-MBP.sgc.devertex.com>
2022-09-24 21:36:41 -0700

    fix: remove external lib

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,6 @@ RUN mkdir /etc/cron.d \
     && ( \
         echo "10 10 * * * safeway-coupons -c /config/accounts >/proc/1/fd/1 2>/proc/1/fd/2" \
         ) >> /var/spool/cron/crontabs/root
-
-RUN pip install python-pushover
 ADD sendmail-pushover.py /
 RUN chmod +x /sendmail-pushover.py && ln -sf /sendmail-pushover.py /usr/bin/sendmail && ln -sf /sendmail-pushover.py /usr/sbin/sendmail
 
--- sendmail-pushover.py
+++ sendmail-pushover.py
@@ -4,15 +4,22 @@
 import os
 from sys import stdin
 import email
-from pushover import Client
+import http.client
+import urllib
 
+pushover_domain = os.environ('PUSHOVER_DOMAIN', 'api.pushover.net')
+pushover_port = os.environ('PUSHOVER_PORT', '443')
 pushover_user = os.environ('PUSHOVER_USER')
 pushover_api_token = os.environ('PUSHOVER_API_TOKEN')
 
 mail = email.message_from_string(stdin.read())
 title = "Message from Sendmail-Pushover"
-if "Subject" in mail:
-    title = mail['Subject']
 
-client = Client(pushover_user, api_token=pushover_api_token)
-client.send_message(mail.as_string(), title=title)
+conn = http.client.HTTPSConnection(f"{pushover_domain}:{pushover_port}")
+conn.request("POST", "/1/messages.json",
+             urllib.parse.urlencode({
+                 "token": pushover_api_token,
+                 "user": pushover_user,
+                 "message": mail.as_string(),
+             }), {"Content-type": "application/x-www-form-urlencoded"})
+conn.getresponse()
